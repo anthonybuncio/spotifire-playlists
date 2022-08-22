aa1ff98e1ffb0952960c7c5bee93456e5b22568a
Anthony Buncio <Anthonyb.ft@gmail.com>
2022-08-22 14:20:00 -0500

    Added DaisyUI React components

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withTM = require('next-transpile-modules')(['react-daisyui'])
+
+const nextConfig = withTM({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["placeimg.com"],
   },
 }
-
+)
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import { Button, Theme } from "react-daisyui";
 
 const Home: NextPage = () => {
   return (
@@ -12,48 +12,8 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <div className="navbar bg-primary text-primary-content rounded-xl">
-          <div className="flex-1">
-            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-          </div>
-          <div className="flex-none gap-2">
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="Search"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <Image
-                    src="https://placeimg.com/80/80/people"
-                    alt="icon"
-                    layout="fill"
-                  />
-                </div>
-              </label>
-              <ul
-                tabIndex={0}
-                className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-              >
-                <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li>
-                  <a>Settings</a>
-                </li>
-                <li>
-                  <a>Logout</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <Button color="primary">Click me!</Button>
+
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </div>
